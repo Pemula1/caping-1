02423e30c8d90dc97d0a8a8ce2da5d1b699c1771
Pemula1 <59829400+Pemula1@users.noreply.github.com>
2020-01-21 19:05:56 +0700

    Update run.php

--- run.php
+++ run.php
@@ -16,12 +16,12 @@ while($i<$jumlah){
 			
 		$news		=	news($uid,$n);
 		$video		=	video($uid,$n);
-		$share		=	share($uid,$n);
+		
 		$code		=	code($uid,$n);
 		$klik		=	klik($uid,$n);
 	echo "TASK NEWS  $news\n";
 	echo "TASK VIDEO $video\n";
-	echo "TASK SHARE $share\n";
+	
 	echo "TASK CODE  $code\n";
 	echo "TASK KLIK  $klik\n";
 	
@@ -44,20 +44,6 @@ function video($uid,$n){
 		$rand = rand(1,9999);
 		$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/video/view/$rand"); 
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-				$headers = array();
-					$headers[] = "Cookie: u=$uid;n=$n";
-					$headers[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36;CapingNews/4.1.4";
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-	return $result;
-}
-function share($uid,$n){
-		$rand = rand(1,9999);
-		$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/event/share/news/$rand"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 				$headers = array();
